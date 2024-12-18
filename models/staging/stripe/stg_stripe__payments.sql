264cf4da0e701ffd6a09a2161e20e72141509dea
Dr. Sandeep Vidyapu <sandy.apj911@gmail.com>
2024-12-18 20:05:54 +0000

    correct the column name passage error to the macro

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,1 +1,7 @@
-select * from {{ source('stripe', 'payment') }}
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    {{ cents_to_dollars('amount') }} as amount
+from {{ source("stripe", "payment") }}
