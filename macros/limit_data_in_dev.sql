9681c04e37247955bfaabfd8d90ec0998f34f8ef
Dr. Sandeep Vidyapu <sandy.apj911@gmail.com>
2024-12-19 07:09:06 +0000

    correct syntax errors of interval for bigquery

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -1,5 +1,5 @@
 {% macro limit_data_in_dev(col_name, last_days) %}
 {% if target.name == 'default' -%}
-where {{ col_name }} >= current_timestamp - interval '{{ last_days }} day'
+where {{ col_name }} >= current_datetime - interval {{ last_days }} day
 {% endif %}
 {% endmacro %}
