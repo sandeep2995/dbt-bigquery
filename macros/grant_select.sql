2317199ccfb712c5e62abb59070cf6af08952536
Dr. Sandeep Vidyapu <sandy.apj911@gmail.com>
2024-12-19 22:00:13 +0000

    correct privilige granting issues of bigquery

--- macros/grant_select.sql
+++ macros/grant_select.sql
@@ -1,9 +1,14 @@
-{% macro grant_select(schema=target.schema, role=target.role) %}
+{% macro grant_select(project=target.project, dataset=target.dataset) %}
     {% set sql %}
+        {{ log('This kind of setup is not possible for bigquery', info=True) }}
+        SELECT "Sorry, Such privilige granting is not possible with me, BigQuery!" AS message;
+        {#
         grant usage on schema {{ schema }} to role {{ role }};
         grant select on all tables in schema {{ schema }} to role {{ role }};
         grant usaselect on all views in schema {{ schema }} to role {{ role }};
+        #}
     {% endset %}
-    {{ log('Executing the sql statement...', info=True) }}
+    {{ log("Executing the sql statement...", info=True) }}
     {% do run_query(sql) %}
+    {{ log("Previliges successfully granted...", info=True) }}
 {% endmacro %}
