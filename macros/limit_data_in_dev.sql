200b809bc76a1ed7ac0bf672c2da84a646213599
Dr. Sandeep Vidyapu <sandy.apj911@gmail.com>
2024-12-19 07:33:26 +0000

    update macro with default value provided to the last_days argument

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -1,4 +1,4 @@
-{% macro limit_data_in_dev(col_name, last_days) %}
+{% macro limit_data_in_dev(col_name, last_days=7) %}
 {% if target.name == 'default' -%}
 where {{ col_name }} >= current_datetime - interval {{ last_days }} day
 {% endif %}
