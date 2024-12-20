f890d3e242aca572befc9eeee5440c2be53866cf
Dr. Sandeep Vidyapu <sandy.apj911@gmail.com>
2024-12-20 09:44:46 +0000

    add payment status to the fact orders

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,4 +1,4 @@
-select order_id, customer_id, amount
+select order_id, customer_id, p.status as payment_status, amount
 from {{ ref("stg_jaffle_shop__customers") }} c
 join {{ ref('stg_jaffle_shop__orders') }} o using (customer_id)
 join {{ ref("stg_stripe__payments") }} p using (order_id)
